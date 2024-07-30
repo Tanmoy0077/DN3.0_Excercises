ce083fee16378154dfff4db8ff4e582a88357131
Tanmoy0077 <tanmoy.pradhan0077@gmail.com>
2024-07-30 20:18:45 +0530

    Added logger

--- Week_1_Excercises/Design Patterns and Principles/Singleton_Pattern/src/App.java
+++ Week_1_Excercises/Design Patterns and Principles/Singleton_Pattern/src/App.java
@@ -1,5 +1,17 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        Logger logger1 = Logger.getInstance();
+        Logger logger2 = Logger.getInstance();
+
+        // Logging some messages
+        logger1.log("This is the first log message.");
+        logger2.log("This is the second log message.");
+
+        // Checking if both references point to the same instance
+        if (logger1 == logger2) {
+            System.out.println("Both logger1 and logger2 are the same instance.");
+        } else {
+            System.out.println("logger1 and logger2 are different instances.");
+        }
     }
 }
