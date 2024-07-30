270edab197089cae6d8f6388682fa73c995f969a
Tanmoy0077 <tanmoy.pradhan0077@gmail.com>
2024-07-30 20:32:18 +0530

    Added Factory

--- Week_1_Excercises/Design Patterns and Principles/Factory_Method_Pattern/src/App.java
+++ Week_1_Excercises/Design Patterns and Principles/Factory_Method_Pattern/src/App.java
@@ -1,5 +1,21 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        DocumentFactory wordFactory = new WordDocumentFactory();
+        Document wordDoc = wordFactory.createDocument();
+        wordDoc.open();
+        wordDoc.save();
+        wordDoc.close();
+
+        DocumentFactory pdfFactory = new PdfDocumentFactory();
+        Document pdfDoc = pdfFactory.createDocument();
+        pdfDoc.open();
+        pdfDoc.save();
+        pdfDoc.close();
+
+        DocumentFactory excelFactory = new ExcelDocumentFactory();
+        Document excelDoc = excelFactory.createDocument();
+        excelDoc.open();
+        excelDoc.save();
+        excelDoc.close();
     }
 }
