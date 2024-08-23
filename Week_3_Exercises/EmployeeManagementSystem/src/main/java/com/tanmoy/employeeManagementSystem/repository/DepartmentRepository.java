83044a22f1a9164685c967e62a8a35f284383e58
Tanmoy0077 <tanmoy.pradhan0077@gmail.com>
2024-08-23 19:45:37 +0530

    Updated Folder Structure

--- Week_3_Exercises/EmployeeManagementSystem/src/main/java/com/tanmoy/employeeManagementSystem/repository/DepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tanmoy.employeeManagementSystem.repository;
-
-import com.tanmoy.employeeManagementSystem.entity.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DepartmentRepository extends JpaRepository<Department, Integer> {
-}
